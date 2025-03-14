772be88716268ab92c880cc1ee11f917a072a483
mtddev <mtddev2004@gmail.com>
2025-03-15 10:53:18 +1100

    Add router setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
 import NavBar from "./components/NavBar";
 import Root from "./components/Root";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
 
 function App() {
   return (
     <Root>
       <NavBar />
+      <Router>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/about" element={<HomePage />} />
+          <Route path="*" element={<HomePage />} /> {/* 404 Page */}
+        </Routes>
+      </Router>
     </Root>
   );
 }
